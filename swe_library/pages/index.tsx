3f0bbbcafc63931d0a5419bf21f4e44857140795
Tony <tonymeissner70@gmail.com>
2023-10-09 02:57:04 +0200

    ui changes

--- swe_library/pages/index.tsx
+++ swe_library/pages/index.tsx
@@ -7,53 +7,62 @@ import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 import React from "react";
-import { Button, Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Image,
+} from "@nextui-org/react";
 import { useRouter } from "next/navigation";
-import { BiSearch } from 'react-icons/bi';
-import { ImBooks } from 'react-icons/im';
+import { BiSearch } from "react-icons/bi";
+import { ImBooks } from "react-icons/im";
 
 export default function IndexPage() {
   const router = useRouter();
 
   return (
     <DefaultLayout>
-      <main>
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-          <div className="grid grid-cols-2 gap-2 sm:grid-cols-2">
-            <Card
-              shadow="sm"
-              key="1"
-              isPressable
-              onPress={() => router.push("/search")}
-            >
-              <CardBody className="overflow-visible p-0">
-                <Button radius="lg" className="h-[140px] w-full object-cover" onPress={() => router.push("/search")}>
-                  <BiSearch />
-                </Button>
-              </CardBody>
-              <CardFooter className="justify-between text-small">
-                <b>Medium suchen</b>
-              </CardFooter>
-            </Card>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <div className="grid grid-cols-2 gap-2 sm:grid-cols-2">
+          <Card
+            shadow="sm"
+            key="1"
+            isPressable
+            isHoverable
+            className="h-[250px] w-[300px]"
+            onPress={() => router.push("/search")}
+          >
+            <CardBody className="">
+              <BiSearch className="h-full w-full" />
+            </CardBody>
+            <Divider />
+            <CardFooter className="">
+              <h4 className="text-large font-bold">Medium suchen</h4>
+            </CardFooter>
+          </Card>
 
-            <Card
-              shadow="sm"
-              key="1"
-              isPressable
-              onPress={() => router.push("/ausleihen")}
-            >
-              <CardBody className="overflow-visible p-0">
-                <Button radius="lg" className="h-[140px] w-full object-cover" onPress={() => router.push("/ausleihen")}>
-                  <ImBooks />
-                </Button>
-              </CardBody>
-              <CardFooter className="justify-between text-small">
-                <b>ausgeliehene Medien anzeigen</b>
-              </CardFooter>
-            </Card>
-          </div>
-        </section>
-      </main>
+          <Card
+            shadow="sm"
+            key="1"
+            isPressable
+            isHoverable
+            className="h-[250px] w-[300px]"
+            onPress={() => router.push("/ausleihen")}
+          >
+            <CardBody className="">
+              <ImBooks className="h-full w-full" />
+            </CardBody>
+            <Divider />
+            <CardFooter className="">
+              <h4 className="text-large font-bold">
+                ausgeliehene Medien anzeigen
+              </h4>
+            </CardFooter>
+          </Card>
+        </div>
+      </section>
     </DefaultLayout>
   );
 }
