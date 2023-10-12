ab311aa16e582c528569c13d9a7ce431565682f8
Tony <tonymeissner70@gmail.com>
2023-10-12 14:51:01 +0200

    removed navbar stuff

--- swe_library/components/navbar.tsx
+++ swe_library/components/navbar.tsx
@@ -1,9 +1,6 @@
 "use client";
 import {
-  Button,
-  Kbd,
   Link,
-  Input,
   Navbar as NextUINavbar,
   NavbarContent,
   NavbarMenu,
@@ -19,32 +16,9 @@ import { siteConfig } from "@/config/site";
 import NextLink from "next/link";
 import clsx from "clsx";
 
-import { SearchIcon } from "@/components/icons";
-
 import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: "bg-default-100",
-        input: "text-sm",
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={["command"]}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="pointer-events-none flex-shrink-0 text-base text-default-400" />
-      }
-      type="search"
-    />
-  );
-
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -72,19 +46,11 @@ export const Navbar = () => {
         </div>
       </NavbarContent>
 
-      <NavbarContent
-        className="hidden basis-1/5 sm:flex sm:basis-full"
-        justify="end"
-      >
-        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-      </NavbarContent>
-
       <NavbarContent className="basis-1 pl-4 sm:hidden" justify="end">
         <NavbarMenuToggle />
       </NavbarContent>
-
+      
       <NavbarMenu>
-        {searchInput}
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
--- swe_library/config/site.ts
+++ swe_library/config/site.ts
@@ -1,27 +1,27 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Library",
-	description: "Library Management System",
-	siteUrl: "http://localhost:3000",
-	navItems: [
-	{
-		label: "Home",
-		href: "/",
-	}
-	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
-	],
+  name: "Library",
+  description: "Library Management System",
+  siteUrl: "http://localhost:3000",
+  navItems: [
+    {
+      label: "Mitarbeiterlogin",
+      href: "/employee-login",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Mitarbeiterlogin",
+      href: "/employee-login",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
 };
