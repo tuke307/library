6984c9d6fd21c033c98f6aefbb1ae9f81cd845d1
Tony <tonymeissner70@gmail.com>
2023-10-19 13:59:27 +0200

    fix

--- swe_library/app/api/auth/[...nextauth]/route.ts
+++ swe_library/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@auth/prisma-adapter";
+//import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient, Employee } from "@prisma/client";
 import { randomBytes, randomUUID } from "crypto";
 import { login } from "@/lib/auth";
