98d4609673cf361ed0507e87a317959701a32d4c
Tony <tonymeissner70@gmail.com>
2023-10-28 12:28:21 +0200

    added card in create user form

--- swe_library/app/user/components/addUserForm.tsx
+++ swe_library/app/user/components/addUserForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { Input } from "@nextui-org/react";
+import { Card, CardBody, CardHeader, Divider, Input } from "@nextui-org/react";
 import { experimental_useFormState as useFormState } from "react-dom";
 import { createUser } from "@/actions/user";
 import { SubmitButton } from "./submitButton";
@@ -14,64 +14,72 @@ export default function AddUserForm() {
 
   return (
     <form action={formAction}>
-      <div className="grid grid-cols-2 gap-2">
-        <Input
-          isRequired
-          type="text"
-          label="Nachname"
-          placeholder="Enter your name"
-          name="lastName"
-        />
-        <Input
-          isRequired
-          type="text"
-          label="Vorname"
-          placeholder="Enter your Vorname"
-          name="firstName"
-        />
+      <Card shadow="md">
+        <CardHeader className="flex gap-3">
+          <h1 className="text-3xl">Nutzer hinzufügen</h1>
+        </CardHeader>
+        <Divider />
+        <CardBody>
+          <div className="grid grid-cols-2 gap-2">
+            <Input
+              isRequired
+              type="text"
+              label="Nachname"
+              placeholder="Enter your name"
+              name="lastName"
+            />
+            <Input
+              isRequired
+              type="text"
+              label="Vorname"
+              placeholder="Enter your Vorname"
+              name="firstName"
+            />
 
-        <Input
-          isRequired
-          type="email"
-          label="Email"
-          placeholder="Enter your email"
-          name="email"
-        />
-      </div>
+            <Input
+              isRequired
+              type="email"
+              label="Email"
+              placeholder="Enter your email"
+              name="email"
+            />
+          </div>
 
-      <div className="mt-8 grid grid-cols-2 gap-2">
-        <Input
-          isRequired
-          type="text"
-          label="Straße"
-          placeholder="Enter your Straße"
-          name="street"
-        />
+          <div className="mt-8 grid grid-cols-2 gap-2">
+            <Input
+              isRequired
+              type="text"
+              label="Straße"
+              placeholder="Enter your Straße"
+              name="street"
+            />
 
-        <Input
-          isRequired
-          type="text"
-          label="Hausnummer"
-          placeholder="Gib deine Hausnummer ein"
-          name="houseNumber"
-        />
+            <Input
+              isRequired
+              type="text"
+              label="Hausnummer"
+              placeholder="Gib deine Hausnummer ein"
+              name="houseNumber"
+            />
 
-        <Input
-          isRequired
-          type="number"
-          label="PLZ"
-          placeholder="Enter your PLZ"
-          name="plz"
-        />
+            <Input
+              isRequired
+              type="number"
+              label="PLZ"
+              placeholder="Enter your PLZ"
+              name="plz"
+            />
 
-        <Input
-          isRequired
-          type="text"
-          label="Stadt"
-          placeholder="Enter your Stadt"
-          name="city"
-        />
-      </div>
+            <Input
+              isRequired
+              type="text"
+              label="Stadt"
+              placeholder="Enter your Stadt"
+              name="city"
+            />
+          </div>
+        </CardBody>
+      </Card>
 
       <div>
         <SubmitButton />
--- swe_library/app/user/create/page.tsx
+++ swe_library/app/user/create/page.tsx
@@ -5,8 +5,6 @@ import AddUserForm from "../components/addUserForm";
 export default async function CreateUserPage() {
   return (
     <section className="m-10">
-      <h1 className="my-10 text-3xl font-semibold">Nutzer hinzufügen</h1>
-
       <AddUserForm />
     </section>
   );
