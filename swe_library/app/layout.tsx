c66d361087692c68535ffe1087224d79967e83c9
Tony <tonymeissner70@gmail.com>
2023-10-31 11:23:39 +0100

    add toast container

--- swe_library/app/layout.tsx
+++ swe_library/app/layout.tsx
@@ -2,10 +2,11 @@ import "@/styles/globals.css";
 import { Metadata } from "next";
 import { Providers } from "./providers";
 import { Navbar } from "./components/navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 //import { getServerSession } from "next-auth";
 //import SessionProvider from "./components/SessionProvider";
 //import { authOptions } from "./api/auth/[...nextauth]";
-
 
 export const metadata: Metadata = {
   title: "Bibliothek",
@@ -23,12 +24,21 @@ export default function RootLayout({
     <html lang="de" suppressHydrationWarning>
       <head />
       <body>
-     
         <Providers>
+          <ToastContainer
+            position="top-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
           <Navbar />
           {children}
         </Providers>
-       
       </body>
     </html>
   );
