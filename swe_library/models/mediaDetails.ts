1c848ef991fd688da58119ff2049329577efbc79
Tony <tonymeissner70@gmail.com>
2023-10-31 11:24:10 +0100

    specify properties

--- swe_library/models/mediaDetails.ts
+++ swe_library/models/mediaDetails.ts
@@ -1,9 +1,32 @@
+import { Author, Location, Media, MediaType, RentedMedia, User } from "@prisma/client";
 
-import { Author, Location, Media, RentedMedia } from "@prisma/client";
 
-// derivated media model from Media, with additional properties
-export type MediaDetails = Media & {
-  author: Author;
-  location: Location | null;
-  rentedBy: RentedMedia[] | null;
+export type MediaDetailProp = {
+  mediaId: string,
+  mediaTitle: string,
+  mediaMediaType: MediaType,
+  mediaContent: string,
+  mediaPublished: boolean,
+  mediaISBN: string,
+  mediaCreatedAt: Date,
+  mediaUpdatedAt: Date,
+
+  authorId: number | null,
+  authorFirstName: string| null,
+  authorLastName: string| null,
+  authorBirthday: Date | null,
+  authorEmail: string| null,
+
+  locationId:  number | null,
+  locationFloor: number | null,
+  locationShelf: number | null,
+  locationShelfSection: number | null,
+
+  rentedMediaId: number| null,
+  rentedMediaMediaId: string| null,
+  rentedMediaUserId: number| null,
+  rentedMediaUserLastName: string | null,
+  rentedMediaUserFirstName: string | null,
+  rentedMediaRentedDate: Date | null,
+  rentedMediaReturnDate: Date | null,
 };
