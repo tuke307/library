ce3dbe50d20d5f427833817aa44e4764bbc0157d
Tony <tonymeissner70@gmail.com>
2023-10-31 11:24:38 +0100

    add card

--- swe_library/app/employee/login/components/loginForm.tsx
+++ swe_library/app/employee/login/components/loginForm.tsx
@@ -1,16 +1,23 @@
 "use client";
 import React, { useState } from "react";
-import { Button, Input } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Input,
+} from "@nextui-org/react";
 import { AiFillEye } from "react-icons/ai";
 import { AiFillEyeInvisible } from "react-icons/ai";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
 import { SubmitButton } from "@/app/components/submitButton";
+import { toast } from "react-toastify";
 
 export default function LoginForm() {
   const router = useRouter();
-  const [errorText, setErrorText] = useState("");
-  const [loading, setLoading] = useState(false);
   const [isVisible, setIsVisible] = React.useState(false);
   const toggleVisibility = () => setIsVisible(!isVisible);
   const [formData, setFormData] = useState({
@@ -19,7 +26,6 @@ export default function LoginForm() {
   });
 
   function handleChange(event: React.ChangeEvent<any>) {
-    setErrorText("");
     const { name, value } = event.target;
     setFormData((prevFormData) => {
       return {
@@ -40,65 +46,68 @@ export default function LoginForm() {
     if (res?.ok) {
       router.push("/");
     } else if (res?.status === 401) {
-      setErrorText(
-        "Die angegebene Mitarbeiternummer oder das Passwort ist ungültig.",
-      );
+      toast.error("Die angegebene Mitarbeiternummer oder das Passwort ist ungültig.");
       formData.password = "";
     } else {
-      setErrorText(
-        "Es ist ein Fehler aufgetreten. Bitte versuche es später erneut",
-      );
+      toast.error("Es ist ein Fehler aufgetreten. Bitte versuche es später erneut");
+
       formData.id = "";
       formData.password = "";
     }
   }
 
   return (
-    <form onSubmit={handleSubmit} className="min-w-[300px]">
-      <div className="grid grid-rows-2 gap-5">
-        <Input
-          isRequired
-          onChange={handleChange}
-          value={formData.id}
-          label="Mitarbeiternummer"
-          type="number"
-          variant="bordered"
-          name="id"
-          labelPlacement="outside"
-          placeholder="Gib deine Mitarbeiternummer ein"
-          className="max-w-xs"
-        />
+    <form onSubmit={handleSubmit} className="w-1/3">
+      <Card >
+        <CardHeader>
+          <h2 className="text-3xl font-bold">Login</h2>
+        </CardHeader>
+        <Divider/>
+        <CardBody className="flex flex-col gap-3">
+          <Input
+            isRequired
+            onChange={handleChange}
+            value={formData.id}
+            label="Mitarbeiternummer"
+            type="number"
+            variant="bordered"
+            name="id"
+            labelPlacement="outside"
+            placeholder="Gib deine Mitarbeiternummer ein"
+            className="flex-grow"
+          />
 
-        <Input
-          isRequired
-          onChange={handleChange}
-          value={formData.password}
-          label="Passwort"
-          variant="bordered"
-          name="password"
-          placeholder="Gib dein Passwort ein"
-          labelPlacement="outside"
-          endContent={
-            <button
-              className="focus:outline-none"
-              type="button"
-              onClick={toggleVisibility}
-            >
-              {isVisible ? (
-                <AiFillEyeInvisible className="pointer-events-none text-2xl text-default-400" />
-              ) : (
-                <AiFillEye className="pointer-events-none text-2xl text-default-400" />
-              )}
-            </button>
-          }
-          type={isVisible ? "text" : "password"}
-          className="max-w-xs"
-        />
-      </div>
-
-      
-      <SubmitButton text="Login" />
-      <p className="mb-4 text-center text-base text-red-500">{errorText}</p>
+          <Input
+            isRequired
+            onChange={handleChange}
+            value={formData.password}
+            label="Passwort"
+            variant="bordered"
+            name="password"
+            placeholder="Gib dein Passwort ein"
+            labelPlacement="outside"
+            endContent={
+              <button
+                className="focus:outline-none"
+                type="button"
+                onClick={toggleVisibility}
+              >
+                {isVisible ? (
+                  <AiFillEyeInvisible className="pointer-events-none text-2xl text-default-400" />
+                ) : (
+                  <AiFillEye className="pointer-events-none text-2xl text-default-400" />
+                )}
+              </button>
+            }
+            type={isVisible ? "text" : "password"}
+            className="flex-grow"
+          />
+        </CardBody>
+        <Divider/>
+        <CardFooter>
+          <SubmitButton text="Login" />
+        </CardFooter>
+      </Card>
     </form>
   );
 }
